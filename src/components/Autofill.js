703ff0fa5d78aa603a916c46dd9bf3fb81e59be8
Adithya <66507607+SlaYpoint@users.noreply.github.com>
2021-12-27 18:19:02 +0530

    DOC : readme and comment

---
+++ src/components/Autofill.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import SuggestionsList from "./SuggestionsList";
+
+const Autofill = ({ suggestions }) => {
+  const [filteredSuggestions, setFilteredSuggestions] = useState([]);
+  const [showSuggestions, setShowSuggestions] = useState(false);
+  const [activeSuggestionIdx, setActiveSuggestionIdx] = useState(0);
+  const [input, setInput] = useState("");
+
+  const inputHandler = (e) => {
+    const userInput = e.target.value;
+
+    const filteredOut = suggestions.filter(
+      (suggestion) =>
+        suggestion.toLowerCase().indexOf(userInput.toLowerCase()) > -1
+    );
+
+    setInput(e.target.value);
+    setFilteredSuggestions(filteredOut);
+    setActiveSuggestionIdx(0);
+    setShowSuggestions(true);
+  };
+
+  // on selecting the suggestion from the drop-down
+  const selectHandler = (e) => {
+    setFilteredSuggestions([]);
+    setInput(e.target.innerText);
+    setActiveSuggestionIdx(0);
+    setShowSuggestions(false);
+  };
+
+  return (
+    <>
+      <input
+        type="text"
+        placeholder="start typing..."
+        onChange={inputHandler}
+        value={input}
+      />
+      {showSuggestions && input && (
+        <SuggestionsList
+          filteredSuggestions={filteredSuggestions}
+          activeSuggestionIndex={activeSuggestionIdx}
+          onClickHandler={selectHandler}
+        />
+      )}
+    </>
+  );
+};
+
+export default Autofill;
