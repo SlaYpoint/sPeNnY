6e0ab5e1b232d76b546bf346b8a2d57a9533a123
SlaYpoint <codersaysady@gmail.com>
2021-12-27 19:01:42 +0530

    FIX : on key down

--- autofill-app/src/components/Autofill.js
+++ autofill-app/src/components/Autofill.js
@@ -27,12 +27,38 @@ const Autofill = ({ suggestions }) => {
     setShowSuggestions(false);
   };
 
+  const onKeyDown = (e) => {
+    let idx;
+    switch(e.keyCode) {
+      case 38:
+        idx = activeSuggestionIdx;
+        if(idx - 1 !== 0)
+          setActiveSuggestionIdx(idx - 1);
+        break;
+      case 40:
+        idx = activeSuggestionIdx;
+        if(idx + 1 !== suggestions.length)
+          setActiveSuggestionIdx(idx + 1);
+        break;
+      case 13:
+        setInput(suggestions[activeSuggestionIdx]);
+        setFilteredSuggestions([]);
+        setActiveSuggestionIdx(0);
+        setShowSuggestions(false);
+        break;
+      default :
+        // do
+        break;
+    }
+  };
+
   return (
     <>
       <input 
         type="text"
         placeholder="start typing..."
         onChange={inputHandler}
+        onKeyDown={onKeyDown}
         value={input}
       />
       {showSuggestions &&
